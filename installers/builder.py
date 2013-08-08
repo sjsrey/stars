971e5ec182569375c368917e3a7da4fd584f1f87
Serge Rey <sjsrey@gmail.com>
2013-08-07 21:41:03 -0700

    - first version of mac builder done

--- installers/builder.py
+++ installers/builder.py
@@ -5,4 +5,20 @@ if not os.path.exists("mac"):
 
 cmd ="cd mac;tar xzvf ../pyinstaller-pyinstaller-v2.0-518-gcd90936.tar.gz "
 os.system(cmd)
+cmd ="cd mac;cp -R  pyinstaller-pyinstaller-cd90936/* ."
+os.system(cmd)
+cmd ="cd mac; python pyinstaller.py --windowed --name=stars ../../stars/starsgui.py"
+os.system(cmd)
+
+targetPath = "mac/stars/dist/stars.app/Contents/MacOS/."
+cmds = ["cp ../../stars/stars/splash.gif",
+        "cp -R ../../stars/stars/data",
+        "cp ../../stars/stars/COPYING",
+        "cp ../../stars/stars/credits.txt"]
+
+for cmd in cmds:
+    cmd = cmd + " " + targetPath
+    print cmd
+    os.system(cmd)
+
 
