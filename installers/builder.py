445298e72c0b4f304693677cb8c9a4f2df814f94
Sergio Rey <sjsrey@gmail.com>
2013-08-08 21:47:49 -0700

    handler for opts

--- installers/builder.py
+++ installers/builder.py
@@ -1,25 +1,69 @@
 import os
+import sys
+import getopt
 
-# add function for mac and another for windows
-if not os.path.exists("mac"):
-    os.makedirs("mac")
+def mac():
+    if not os.path.exists("mac"):
+	os.makedirs("mac")
 
-cmd ="cd mac;tar xzvf ../pyinstaller-pyinstaller-v2.0-518-gcd90936.tar.gz "
-os.system(cmd)
-cmd ="cd mac;cp -R  pyinstaller-pyinstaller-cd90936/* ."
-os.system(cmd)
-cmd ="cd mac; python pyinstaller.py --windowed --name=stars ../../stars/starsgui.py"
-os.system(cmd)
+	cmd ="cd mac;tar xzvf ../pyinstaller-pyinstaller-v2.0-518-gcd90936.tar.gz "
+	os.system(cmd)
+	cmd ="cd mac;cp -R  pyinstaller-pyinstaller-cd90936/* ."
+	os.system(cmd)
+	cmd ="cd mac; python pyinstaller.py --windowed --name=stars ../../stars/starsgui.py"
+	os.system(cmd)
 
-targetPath = "mac/stars/dist/stars.app/Contents/MacOS/."
-cmds = ["cp ../../stars/stars/splash.gif",
-        "cp -R ../../stars/stars/data",
-        "cp ../../stars/stars/COPYING",
-        "cp ../../stars/stars/credits.txt"]
+	targetPath = "mac/stars/dist/stars.app/Contents/MacOS/."
+	cmds = ["cp ../../stars/stars/splash.gif",
+		"cp -R ../../stars/stars/data",
+		"cp ../../stars/stars/COPYING",
+		"cp ../../stars/stars/credits.txt"]
 
-for cmd in cmds:
-    cmd = cmd + " " + targetPath
-    print cmd
-    os.system(cmd)
+	for cmd in cmds:
+	    cmd = cmd + " " + targetPath
+	    print cmd
+	    os.system(cmd)
 
+def win():
+    if not os.path.exists("win"):
+	os.makedirs("win")
+
+	cmd ="cd win;cp -R  ../pyinstaller-pyinstaller-cd90936/* ."
+	os.system(cmd)
+	cmd ="cd win; python pyinstaller.py --windowed --name=stars ../../stars/starsgui.py"
+	os.system(cmd)
+
+	targetPath = "win/stars/dist/stars.app/Contents/winOS/."
+	cmds = ["cp ../../stars/stars/splash.gif",
+		"cp -R ../../stars/stars/data",
+		"cp ../../stars/stars/COPYING",
+		"cp ../../stars/stars/credits.txt"]
+
+	for cmd in cmds:
+	    cmd = cmd + " " + targetPath
+	    print cmd
+	    os.system(cmd)
+
+dispatcher = {}
+dispatcher['mac'] = mac
+dispatcher['win'] = win
+
+def main(argv):
+    opts, args = getopt.getopt(argv,"ht:") 
+    for o,a in opts:
+	if o == '-h':
+	    print 'useage: builder.py -t [mac|win]'
+	    sys.exit(0)
+	elif o == "-t":
+	    target = a
+	    print "target: ", target
+	    if target in dispatcher:
+		dispatcher()
+	    else:
+		print 'useage: builder.py -t [mac|win]'
+	else:
+	    print 'useage: builder.py -t [mac|win]'
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
